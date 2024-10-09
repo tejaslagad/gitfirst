40a6d7f2ed6e43ce8ec5944cef53895f3f860de3
Ubuntu <ubuntu@ip-172-31-5-14.ap-south-1.compute.internal>
2024-10-09 10:56:23 +0000

    shell

---
+++ day02/vaibhulal_ki_duniya.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# this is vaibhulal ki duniya
+<< comment
+anything
+writen
+here will be not execute
+comment
+
+name="kallu ka vaibhu"
+
+echo "name is $name, and current directory is $(pwd)"
